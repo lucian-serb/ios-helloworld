dae163fb56cfafc8af81386cd5e0fabf042abd44
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-04 00:58:22 +0300

    Implement the changeGreeting: method
    Get the text from the text field and set the userName propriety (model object) to its value. Create a new NSString from the model object and test if its length is 0. If it is then put “World” as default NSString. Finally construct the greeting and set the label text to its value.

--- HelloWorld/HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorld/HelloWorldViewController.m
@@ -30,5 +30,14 @@
 }
 
 - (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@ !!!", nameString];
+    self.label.text = greeting;
 }
 @end
