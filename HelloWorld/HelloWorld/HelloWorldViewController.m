24c5cf3a8a6beef2b4a7a4e8dbd29e3eaee6d225
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-19 23:42:50 +0300

    Call the NSLocalizedString macro with the key HELLO

--- HelloWorld/HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorld/HelloWorldViewController.m
@@ -37,7 +37,7 @@
         nameString = @"World";
     }
     
-    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@ !!!", nameString];
+    NSString *greeting = [[NSString alloc] initWithFormat:NSLocalizedString(@"HELLO", @"The string displayed"), nameString];
     self.label.text = greeting;
 }
 
